79e59ee4f57e68235ce27e455cae5502acf45341
Alex Mitchell <shamsup@users.noreply.github.com>
2016-10-15 11:05:08 -0500

    Remove makerpass data from schemas, add knexfile example

---
+++ knexfile.example.js
@@ -0,0 +1,49 @@
+// Be sure to update with your config settings and save as knexfile.js
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      database: 'hackerpaste_dev',
+      user:     'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  test: {
+    client: 'pg',
+    connection: {
+      database: 'hackerpaste_test',
+      user:     'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'hackerpaste',
+      user:     'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+};
--- migrations/20161014164216_initial.js
+++ migrations/20161014164216_initial.js
@@ -1,53 +1,32 @@
-
 exports.up = function(knex, Promise) {
   return Promise.all([
-    knex.schema.createTable('users', function(table) {
-      table.string('id').primary();
-    }),
-    knex.schema.createTable('groups', function(table) {
-      table.string('id').primary();
-    }),
-    knex.schema.createTable('sessions', function(table) {
-      table.string('id').primary();
-      table.string('user_id').references('users.id');
-    }),
     knex.schema.createTable('pasties', function(table) {
-      table.string('id').primary();
-      table.string('user_id');
-      table.string('title');
-      table.text('contents');
-      table.string('type', 10);
-      table.boolean('public').defaultTo(false);
-      table.timestamps(false, true);
-    }),
-    knex.schema.createTable('tags', function(table) {
       table.increments('id');
-      table.string('name');
+      table.string('user_uid'); // Makerpass user_uid
+      table.string('title');
+      table.text('contents'); // raw text input from user
+      table.text('contents_parsed') // For server-side parsing and storing the result
+      table.string('file_type', 10); // Potential future for syntax highlighting/parsing
+      table.specificType('tags', 'varchar[]'); // array data type in postgres
+      table.boolean('public').defaultTo(false); // flag for publicly viewable
+      table.timestamps(false, true); // datetime columns with defaults
     }),
-    knex.schema.createTable('pasties_groups', function(table) {
-      table.string('pastie_id').references('pasties.id');
-      table.string('group_id').references('groups.id');
+    knex.schema.createTable('pasties_subjects', function(table) {
+      table.string('pastie_id');
+      table.string('subject_uid'); // group_/user_uid from Makerpass, depends on subject_type
+      table.string('subject_type'); // 'Group' for now, 'User' and more in the future
     }),
     knex.schema.createTable('favorites', function(table) {
-      table.string('user_id').references('users.id');
-      table.string('pastie_id').references('pasties.id');
-    }),
-    knex.schema.createTable('pasties_tags', function(table) {
-      table.string('pastie_id').references('pasties.id');
-      table.integer('tag_id').references('tags.id');
+      table.string('user_uid')
+      table.string('pastie_id');
     })
   ]);
 };
 
 exports.down = function(knex, Promise) {
   return Promise.all([
-    knex.schema.dropTable('pasties_tags'),
     knex.schema.dropTable('favorites'),
-    knex.schema.dropTable('pasties_groups'),
-    knex.schema.dropTable('tags'),
-    knex.schema.dropTable('groups'),
-    knex.schema.dropTable('sessions'),
-    knex.schema.dropTable('pasties'),
-    knex.schema.dropTable('users')
+    knex.schema.dropTable('pasties_subjects'),
+    knex.schema.dropTable('pasties')
   ]);
 };
