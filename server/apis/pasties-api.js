da4b680cf108a922948eb3b5256e2dc437355732
carsontmoore <carsontmoore@gmail.com>
2016-10-16 14:40:31 -0500

    Mockup Pastie Endpoints (#25)
    * Mockup Pastie Endpoints
    
    * Pastie Endpoints w/initial corrections
    
    * Pastie Endpoints Spacing Fixed

--- server/apis/pasties-api.js
+++ server/apis/pasties-api.js
@@ -10,3 +10,36 @@ PastiesAPI.get('/pasties/:id', function (req, res) {
     .catch( Pastie.NotFound, API.prep(404, res) )
     .catch( API.catchUnexpectedErrors )
 })
+
+
+PastiesAPI.get('/pasties', function(req, res) {
+  
+  Pastie.feedForUser( req.user.uid, req.query )
+    .then( API.prep(200, res) )
+    .catch( Pastie.NotFound, API.prep(404, res) )
+    .catch( API.catchUnexpectedErrors )
+})
+
+
+PastiesAPI.get('/pasties/public', function(req, res) {
+  
+  Pastie.getPublic( req.query )
+    .then( API.prep(200, res) )
+    .catch( Pastie.NotFound, API.prep(404, res) )
+    .catch( API.catchUnexpectedErrors )
+})
+
+PastiesAPI.post('/pasties', function(req, res) {
+  
+  Pastie.create( req.body )
+    .then( API.prep(201, res) )
+    .catch( Pastie.InvalidFormat, API.prep(400, res) )
+    .catch( API.catchUnexpectedErrors)
+})
+
+// PastiesAPI.post('/groups/:id/pasties') {
+
+// 	Pastie.create(req.body)
+// 		.then(API.prep(200, res))
+// 		.catch( API.catchUnexpectedErrors)
+// }
