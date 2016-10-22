d5c8736b516e06b33881b3f431b80d89d54831d0
Arvin Wallace <arvinwallace@gmail.com>
2016-10-22 14:05:02 -0500

    Share model (#63)
    * Share.create
    
    * Share.delete

---
+++ server/models/share.js
@@ -0,0 +1,49 @@
+var db = require('../lib/db');
+
+var Share = module.exports;
+
+
+
+Share.create = function(pastie_id, subjects){
+	var shares = subjects.map(function(sub){
+	  return {
+	    pastie_id: pastie_id,
+	    subject_type: sub.subject_type,
+	    subject_uid: sub.subject_uid
+	  };
+	});
+	return db('pasties_subjects').insert(shares)
+	.then((result) => result);
+}
+
+
+
+Share.delete = function (pastie_id, subjects) {
+  var shares = subjects.map(subject => ({
+    pastie_id: pastie_id,
+    subject_type: subject.subject_type,
+    subject_uid: subject.subject_uid
+  }));
+  ///  this creates an SQL query that delets all subjects passed in
+  ///   delete from pastie_subjects where pastie_id = 123 and (...)
+  return db('pasties_subjects').where('pastie_id', pastie_id).andWhere(function() {
+    /// this loops through all of the shares adding an 'orWhere' clause
+    ///  (subject_type = 'User' and subject_id = 'user_alice')
+    /// or (subject_type = 'Group' and subject_id = 'group_1')
+    shares.forEach((share) => {
+      this.orWhere({
+        subject_type: share.subject_type,
+        subject_uid: share.subject_uid
+      });
+    });
+  }).del()
+  .then((result) => result);
+}
+
+
+
+
+
+
+
+
