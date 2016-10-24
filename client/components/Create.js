7303beaa561877d02ab88f993034763e0ea4ad69
ArtyEmsee <rdmcelligott@gmail.com>
2016-10-24 12:29:55 -0500

    Style Create Page (#85)

--- client/components/Create.js
+++ client/components/Create.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var axios = require('axios')
+var route = require('page')
 
 module.exports =  class Create extends React.Component {
   constructor(props) {
@@ -9,8 +10,10 @@ module.exports =  class Create extends React.Component {
     this.state = {
       contents: "",
       title: "",
-      public: false
+      public: false,
+      loading: false
     };
+
     this.handleContentsChange = this.handleContentsChange.bind(this);
     this.handleTitleChange = this.handleTitleChange.bind(this);
     this.handlePublicChange = this.handlePublicChange.bind(this);
@@ -31,6 +34,7 @@ module.exports =  class Create extends React.Component {
   }
 
   handleSubmit(event) {
+    this.setState({loading: true})
     axios({
       method: 'post',
       url: '/api/pasties',
@@ -40,17 +44,30 @@ module.exports =  class Create extends React.Component {
         public: this.state.public
       }
       })
+      .then(function(pastie) {
+        console.log("paste: ", pastie.data.id)
+        route.redirect(`/pasties/${pastie.data.id}`)
+      })
+      .catch(function(err) {
+        this.setState({loading: false})
+        console.log(err)
+      })
   }
 
   render() {
     return (
-      <div className="container">
-        <button type="submit" onClick={this.handleSubmit}>Submit</button>
-        <input type="text" className="pastieTitle" placeholder="enter title" onChange={this.handleTitleChange}/>
-        <input type="checkbox" className="pastiePublic" checked={this.state.public} onChange={this.handlePublicChange}/>
-        <textarea className="text-input" type="text" placeholder="enter text"
-            value={this.state.value} onChange={this.handleContentsChange}>
-        </textarea>
+      <div className="row">
+        <div className="content">
+
+          <input disabled={this.state.loading} type="text" className="pastieTitle" placeholder="enter title" onChange={this.handleTitleChange}/>
+          <input disabled={this.state.loading} type="checkbox" className="pastiePublic" checked={this.state.public} onChange={this.handlePublicChange}/>
+            <textarea disabled={this.state.loading} className="text-input" type="text" placeholder="enter text"
+                value={this.state.value} onChange={this.handleContentsChange}>
+            </textarea>
+        </div>
+        <div className="aside">
+          <button disabled={this.state.loading} type="submit" className="pastieSubmit" onClick={this.handleSubmit}>Submit</button>
+        </div>
       </div>
     )
   }
